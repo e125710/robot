c1e378d95186efc045fc7965204b90455679a734
Natumi KOUTI <e125710@Natumi-MacBook-Pro.local>
2014-12-29 18:21:10 +0900

    robot->class,rge1.01->Individual

---
+++ rge1.01/example/App.h
@@ -0,0 +1,32 @@
+//
+//  App.h
+//  rge_sample
+//
+//  Created by Yuhei Akamine on 2014/10/28.
+//  Copyright (c) 2014年 Yuhei Akamine. All rights reserved.
+//
+
+#ifndef __rge_sample__App__
+#define __rge_sample__App__
+#include "RGE.h"
+#include "Frame.h"
+
+class App
+{
+public:
+    App();
+    void init();
+    void applyWindowSize(int w, int h);
+    void update();
+    void display();
+    
+    void mouseLeftPressed(int x, int y);
+    void mouseLeftReleased(int x, int y);
+    void mouseLeftDragged(int x, int y);
+private:
+    rge::FrameRef robotBase;
+    
+};
+
+
+#endif /* defined(__rge_sample__App__) */
